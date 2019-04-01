b67c3f40451de63055c4e8b8fd8765ff0eca16dd
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2019-03-31 20:31:33 -0500

    add fetch, trae

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          .field.has-addons
+            .control
+              input.input.is-large(
+                type="text",
+                placeholder="¿Qué canción estás buscando?",
+                v-model="searchQuery"
+              )
+            .control
+              a.button.is-info.is-large(v-on:click="search") Buscar
+            .control
+              a.button.is-danger.is-large &times;
+          p.help.is-info.has-text-right
+            small {{ searchMessage }}
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artists[0].name }}"
 </template>
 
 <script>
+import trackService from './services/track'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      if (this.searchQuery) { return }
+
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+        })
     }
   }
 }
@@ -17,4 +52,7 @@ export default {
 
 <style lang="scss">
   @import './scss/main.scss';
+  .results {
+    margin-top: 50px;
+  }
 </style>
